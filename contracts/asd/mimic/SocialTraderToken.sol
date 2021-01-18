394eda821d7ac0e24b500a16a4ff8ae90b3166ea
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2021-01-17 21:02:54 -0600

    Token update

--- contracts/asd/mimic/SocialTraderToken.sol
+++ contracts/asd/mimic/SocialTraderToken.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
+import "./TraderManager.sol";
 import "../ERC677/ERC677.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract SocialTraderToken is ERC677 {
     using SafeMath for uint256;
+    /**
+     * @dev Address of the trader manager contract
+     */
+     address public TRADER_MANAGER;
     /**
      * @dev Address of the token for fees
      */
@@ -16,6 +21,11 @@ contract SocialTraderToken is ERC677 {
      * FEE = FEE_PER_TOKEN * # of tokens
      */
     uint256 public FEE_PER_TOKEN;
+    /**
+     * @dev Fee for profit taking
+     * Precision of 100.xx (100.00% - 0.00%)
+     */
+    uint16 public PROFIT_TAKE_FEE;
     /**
      * @dev Minimum amount to mint new Social Trader tokens
      */
@@ -29,14 +39,17 @@ contract SocialTraderToken is ERC677 {
      */
     bool public allowNewMints = true;
 
+    event TraderManagerChanged(address _newTraderManager);
+
     constructor(
-        address _socialTrader, 
+        address _TRADER_MANAGER,
         string memory _name, 
         string memory _symbol, 
-        uint8 _decimals,
         address _FEE_TOKEN_ADDRESS,
         uint256 _FEE_PER_TOKEN,
-        uint256 _MINIMUM_MINT
+        uint16 _PROFIT_TAKE_FEE,
+        uint256 _MINIMUM_MINT,
+        address _socialTrader
     ) 
         ERC677(_name, _symbol) 
     {
@@ -48,11 +61,12 @@ contract SocialTraderToken is ERC677 {
                 _FEE_TOKEN_ADDRESS != address(0)
             );
         }
-        socialTrader = _socialTrader;
+        TRADER_MANAGER = _TRADER_MANAGER;
         FEE_TOKEN_ADDRESS = _FEE_TOKEN_ADDRESS;
         FEE_PER_TOKEN = _FEE_PER_TOKEN;
+        PROFIT_TAKE_FEE = _PROFIT_TAKE_FEE;
         MINIMUM_MINT = _MINIMUM_MINT;
-        _setupDecimals(_decimals);
+        socialTrader = _socialTrader;
     }
 
     modifier onlySocialTrader {
@@ -65,6 +79,7 @@ contract SocialTraderToken is ERC677 {
     }
     function burnTokens(uint256 _amount) public {
         _burn(msg.sender, _amount);
+        // Redeem collateral/underlying and profits if any
     }
     function mintTokens(uint256 _amount) public {
         require(
@@ -84,6 +99,9 @@ contract SocialTraderToken is ERC677 {
             token.balanceOf(address(this))
         );
     }
+    function changeFeeToken(address _token) public onlySocialTrader {
+        FEE_TOKEN_ADDRESS = _token;
+    }
     function changeFee(uint256 _amount) public onlySocialTrader {
         if(_amount > 0) {
             require(
@@ -92,8 +110,29 @@ contract SocialTraderToken is ERC677 {
         }
         FEE_PER_TOKEN = _amount;
     }
-    function changeFeeToken(address _token) public onlySocialTrader {
-        FEE_TOKEN_ADDRESS = _token;
+    function changeProfitTakeFee(uint16 _percentage) public onlySocialTrader {
+        require(
+            _percentage <= 10000
+        );
+        PROFIT_TAKE_FEE = _percentage;
+    }
+    function openPosition(
+        TraderManager.OptionStyle _style,
+        TraderManager.TradingType _type,
+        address _oTokenAddress
+    ) 
+        public 
+        onlySocialTrader 
+    {
+        
+    }
+    function closePosition(
+        uint256 _positionIndex
+    )
+        public
+        onlySocialTrader
+    {
+
     }
     function isFollower(address _account) public view returns(bool) {
         return balanceOf(_account) > 0;
@@ -115,6 +154,9 @@ contract SocialTraderToken is ERC677 {
             msg.sender, address(this), 
             _minted.mul(FEE_PER_TOKEN)
         );
+    }
+    function _redeemFunds() internal {
+        // Redeems underlying/collateral + profits (if any)
     }
 
 }
