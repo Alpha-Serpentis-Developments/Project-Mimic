a4902dd9ca5d2329fa32786152246576b5b7ec5c
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2021-05-05 13:53:22 -0500

    more changes

--- contracts/mimic/interfaces/ISocialTraderToken.sol
+++ contracts/mimic/interfaces/ISocialTraderToken.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ITraderManager} from "../interfaces/ITraderManager.sol";
 
 interface ISocialTraderToken is ITraderManager {
     error ZeroAddress();
     error Unauthorized_Admin();
     error UnsafeModule_DoesNotExist();
+    error UnsafeModule_Revert();
     error TradingOperationFailed(TradeOperation operation);
     error PredeterminedStrategyExists(string strategy);
 
     function createPredeterminedStrategy(string memory _strategy, TradeOperation[] memory _operations) external;
-    function executeTrade(TradeOperation[] memory _operations) external;
-    function executePredeterminedStrategy(string memory _strategy) external;
-    function collectFees() external;
+    function executeTrade(uint256 _timestamp, TradeOperation[] memory _operations) external;
+    function executePredeterminedStrategy(uint256 _timestamp, string memory _strategy) external;
+    function collectFees(address _token) external;
     function addUnsafeModule(address _module) external;
     function removeUnsafeModule(address _module) external;
-    function interactWithUnsafeModule(address _module, bytes memory _function) external payable returns(bool success, bytes memory returnData);
+    function interactWithUnsafeModule(address _module, bytes memory _function, bool _revertIfUnsuccessful) external payable returns(bool success, bytes memory returnData);
 }
