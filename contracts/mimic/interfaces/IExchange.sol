ceda00a47089386905531c1c04a837b6e5ae30de
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2021-06-07 14:55:33 -0500

    Cleaning for v0.1 branch

--- contracts/mimic/interfaces/IExchange.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.4;
-
-interface IExchange {
-    function tokenExchange(address _inputToken, uint256 _input, address _outputToken) external;
-    function mintOToken(address _inputToken, uint256 _input, address _oToken) external;
-    function burnOToken(address _inputOToken, uint256 _input) external;
-    function redeemCollateral(address _oToken) external;
-}
