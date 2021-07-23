3bf72ea0428916e43a4772b670009f350d68ad26
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2021-07-23 11:10:55 -0500

    Color corrections

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -15,7 +15,7 @@ const FooterContainer = styled.div`
   bottom: 0;
   width: 100%;
   height: 200px;
-  background-color: #35e9ea5c;
+  background-color: #20759D;
 `;
 const SocialStyle = styled.div`
   transition: transform 0.5s;
