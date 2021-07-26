3ec2cd79d71d3f590b443e8d09e00f0c181f5d3a
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2021-07-25 22:32:53 -0500

    Fixed name and colors

--- frontend/src/pages/Managed.js
+++ frontend/src/pages/Managed.js
@@ -10,7 +10,7 @@ export default function Managed(props) {
       style={{
         borderStyle: "none",
         backgroundColor: "#070036",
-        backgroundImage: "linear-gradient(#8b1bef,#35e9ea5c)",
+        backgroundImage: "linear-gradient(#8b1bef,#20759D)",
       }}
     >
       <Table.Body>
