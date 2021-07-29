94d98b9b9d82240ec144629b1794f422bd554df7
Yimeng Yu <yimeng.yu@gmail.com>
2021-07-28 20:56:43 -0700

    modify landing page button styling

--- frontend/src/components/StartManagingBtn.js
+++ frontend/src/components/StartManagingBtn.js
@@ -1,13 +1,19 @@
 import "../App.css";
 
 export default function StartManagingBtn(props) {
-    return (
-        <button
-            className="StartButtons"
-            style={{ verticalAlign: "middle" }}
-            onClick={props.clickManager}
-        >
-            <span>Start Managing</span>
-        </button>
-    );
+  return (
+    <button
+      className="StartButtons"
+      style={{
+        verticalAlign: "middle",
+        width: "280px",
+        height: "50px",
+        marginBottom: "20px",
+        fontSize: "25px",
+      }}
+      onClick={props.clickManager}
+    >
+      <span>Start Managing</span>
+    </button>
+  );
 }
--- frontend/src/components/StartTXBtn.js
+++ frontend/src/components/StartTXBtn.js
@@ -4,7 +4,13 @@ export default function StartTXBtn(props) {
   return (
     <button
       className="StartButtons"
-      style={{ verticalAlign: "middle" }}
+      style={{
+        verticalAlign: "middle",
+        width: "280px",
+        height: "50px",
+        marginBottom: "20px",
+        fontSize: "25px",
+      }}
       onClick={props.clickTrade}
     >
       <span>Start Trading </span>
