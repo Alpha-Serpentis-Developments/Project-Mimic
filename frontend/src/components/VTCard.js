36461f18ef79f8a1de73972785ca67808da613fd
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2021-11-03 17:42:18 -0500

    Minor graphical fix

--- frontend/src/components/VTCard.js
+++ frontend/src/components/VTCard.js
@@ -18,18 +18,18 @@ const VTCardContainer = styled.div`
   border-radius: 10px;
   display: flex;
   flex-direction: column;
-  width: 600px;
+  width: 550px;
   border: 2px solid #000000;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   display: inline-block;
   margin-left: auto;
   margin-right: auto;
   color: white;
-  @media only screen and (max-width: 600px) {
+  @media only screen and (max-width: 1120px) {
     width: 450px;
   }
-  @media only screen and (max-width: 500px) {
-    width: 350px;
+  @media only screen and (max-width: 900px) {
+    width: 375px;
   }
   // &:hover {
   //   background-color: #8a02b2;
