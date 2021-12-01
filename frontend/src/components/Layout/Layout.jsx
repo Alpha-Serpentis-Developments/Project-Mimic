b0c82623ad34d59d16b57fcd89a49529fc18e07b
Jason Diaz Jimenez <vulpothedev@gmail.com>
2021-12-01 17:25:37 -0600

    Frontend fix

---
+++ frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "../Navbar/Navbar";
+
+export default function Layout({ children }) {
+	return (
+		<>
+			<Head>
+				<title>Optional Finance</title>
+				<meta name="description" content="Decentralized Social Trading for Options" />
+				<meta content="Optional" property="og:title" />
+				<meta content="Decentralized Social Trading for Options" property="og:description" />
+				<meta content="https://optional.finance" property="og:url" />
+				<meta content="/optional.png" property="og:image" />
+				<meta content="#e1b0f2" data-react-helmet="true" name="theme-color" />
+				<link rel="icon" href="/optional.svg" />
+			</Head>
+			<div>
+				<Navbar />
+				{children}
+			</div>
+		</>
+	);
+}
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from './../../styles/Navbar.module.scss'
+import Link from 'next/link'
+
+export default function Navbar() {
+	return (
+		<nav className={styles.navbar}>
+			<img className={styles.icon} src="/optional.svg" width="50px" height="60px" />
+			<div className={styles.links}>
+				<Link href="/">
+					<a>Home</a>
+				</Link>
+				<Link href="/trade">
+					<a>Trade</a>
+				</Link>
+				<Link href="/manager">
+					<a>Manager</a>
+				</Link>
+				<Link href="/docs">
+					<a>Docs</a>
+				</Link>
+			</div>
+			<button className={styles.connectbtn}>Connect </button>
+		</nav>
+	);
+}
