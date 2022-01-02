12036013480648e4a2d61533b90e2da72ed7638c
cloudmencloud <enrpost@gmail.com>
2022-01-02 15:07:17 -0300

    Deleted component

--- frontend/src/pages/manager.jsx
+++ frontend/src/pages/manager.jsx
@@ -4,10 +4,9 @@ export default function Manager()
 {
     return(
         <div className={styles.main}>
-            <div>
-                <h1 className={styles.title}>Social tokens you manage </h1>
-            </div>
-            <div>
+        
+            <h1 className={styles.title}>Social tokens you manage </h1>
+            <div className={styles.container}>
                 <div>
                     <div className={styles.div}>
                         <p> Portfolio: </p>
@@ -18,9 +17,33 @@ export default function Manager()
                         { "A" }
                     </div>
                 </div>
-            </div>
-            <div>
-                
+
+                <div className={styles.managediv}>
+                    <div className={styles.flex}>
+                        <p className={styles.bold}>Time</p>
+                        <p> 1-WK </p>
+                        <p> 3-WK </p>
+                        <p> 3-MO </p>
+                    </div>
+                    <div className={styles.flex}>
+                        <p className={styles.bold}> Inflow </p>
+                        <p> $500.00 </p>
+                        <p> $500.00 </p>
+                        <p> $500.00 </p>
+                    </div>
+                    <div className={styles.flex}>
+                        <p className={styles.bold}> Outflow </p>
+                        <p> $230.00 </p>
+                        <p> $230.00 </p>
+                        <p> $230.00 </p>
+                    </div>
+                    <div className={styles.flex}>
+                        <p className={styles.bold}> Fees </p>
+                        <p> $230.00 </p>
+                        <p> $230.00 </p>
+                        <p> $230.00 </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
