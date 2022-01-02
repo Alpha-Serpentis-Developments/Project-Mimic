d9128776c98049a3f872864545d706cf8ac3b076
cloudmencloud <enrpost@gmail.com>
2022-01-01 22:02:06 -0300

    Added new component files

--- frontend/src/pages/manager.tsx
+++ frontend/src/pages/manager.tsx
@@ -3,6 +3,6 @@ import styles from "../styles/Manager.module.scss";
 export default function Manager()
 {
     return(
-        null
+        <h1 className={styles.title}>Social tokens you manage </h1>
     )
 }
